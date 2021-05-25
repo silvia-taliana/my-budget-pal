0822907b5fa9aadfb37b361f30fe7a60e4b29b0a
Silvia Taliana <silviataliana2@gmail.com>
2021-05-25 14:21:36 +0930

    adding basic form to profile page

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,8 +1,42 @@
 import React from "react";
 
+const payCycle = [
+    {
+        label: "Please select",
+        value: "invalid",
+    },
+    {
+        label: "Week",
+        value: "Week",
+    },
+    {
+        label: "Fortnight",
+        value: "Fortnight",
+    },
+    {
+        label: "Month",
+        value: "Month",
+    }
+]
+
 function Profile() {
     return (
-        <div>Profile</div>
+        <div>
+            <h1>Profile Page</h1>
+
+            <form>
+                <label>Income:</label>
+                <input placeholder="$"></input>
+                <label>Per:</label>
+                <select name="payCycle">
+                    {payCycle.map((option) => (
+                        <option key={option.value} value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+                <label>Total weekly saving goal:</label>
+                <input placeholder="$"></input>
+            </form>
+        </div>
     );
 }
 
