387ce268bcf0066b9dcda0b660fd5b8694864cad
Silvia Taliana <silviataliana2@gmail.com>
2021-05-29 12:49:30 +0930

    deploying

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri="https://blooming-island-79334.herokuapp.com/login"
+    redirectUri="https://blooming-island-79334.herokuapp.com/"
     audience={process.env.REACT_APP_AUTH0_AUDIENCE}
     scope="all">
     <App />
