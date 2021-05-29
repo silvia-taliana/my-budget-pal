70ead10e036b1a80be0037e333b2b4b414663beb
Silvia Taliana <silviataliana2@gmail.com>
2021-05-29 13:33:19 +0930

    deploying

--- client/src/components/PrivateRoutes/PrivateRoute.js
+++ client/src/components/PrivateRoutes/PrivateRoute.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
-import Homepage from "../Homepage/Homepage";
+// import Homepage from "../Homepage/Homepage";
 // import Loading from "../Loading/Loading";
 
 const PrivateRoute = ({ component, ...args }) => (
     <Route
         component={withAuthenticationRequired(component, {
-            onRedirecting: () => <Homepage />
+            onRedirecting: () => <div>loading</div>,
+            returnTo: window.location.href
         })}
         {...args}
     />
